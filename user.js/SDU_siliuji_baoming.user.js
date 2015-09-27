b3d02aab00eaaa48c0213445b90272c1759ee645
liuycsd <liuycsd@gmail.com>
2015-09-27 14:39:18 +0800

    user.js/siliuji: add jiangzhudai

--- user.js/SDU_siliuji_baoming.user.js
+++ user.js/SDU_siliuji_baoming.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
-// @name        山东大学四六级报名系统侧边栏显示脚本
+// @name        山东大学部分老系统侧边栏（menu_m.asp）显示脚本
 // @namespace   https://github.com/liuycsd/shell-scripts/tree/master/user.js
-// @description 显示山东大学四六级报名系统侧边栏
+// @description 显示山东大学部分老系统侧边栏（menu_m.asp）
 // @include     http://202.194.15.97:8888/login.jsp
-// @version     0.2.1
+// @include     http://211.86.56.237:8080/xsgl/student/index_menu.jsp
+// @version     0.2.2
 // @grant       none
 // ==/UserScript==
-document.getElementsByName('content1')[0].src='http://202.194.15.97:8888/menu_m.jsp'
+document.getElementsByName('content1')[0].src='menu_m.jsp'
