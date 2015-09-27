6af293616808898d7a3d2c6e8c35e1b537699c23
liuycsd <liuycsd@gmail.com>
2015-09-27 15:36:49 +0800

    userjs: add jzd_nav

---
+++ user.js/SDU_jzd_nav.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        山东大学本科生奖助贷管理系统导航栏显示脚本
+// @namespace   https://github.com/liuycsd/shell-scripts/tree/master/user.js
+// @description 山东大学本科生奖助贷管理系统导航栏显示脚本
+// @include     http://211.86.56.237:8080/*
+// @version     0.2.1
+// @grant       none
+// ==/UserScript==
+(function() {
+  var scriptNode, targ;
+
+  if (typeof show === 'function') {
+    scriptNode = document.createElement('script');
+    scriptNode.type = "text/javascript";
+    scriptNode.textContent = 'show = function(item) {\n  document.getElementById(item).removeAttribute(\'hidden\');\n  openMenu = item;\n  document.getElementById(item).style.visibility=\'visible\';\n}	';
+    targ = (document.getElementsByTagName('head'))[0] || document.body;
+    targ.appendChild(scriptNode);
+  }
+
+  if (typeof hideit === 'function') {
+    scriptNode = document.createElement('script');
+    scriptNode.type = "text/javascript";
+    scriptNode.textContent = 'hideit = function(item) {\n  if(! window[\'in_\'+item]) {\n    openMenu = "";\n    document.getElementById(item).style.visibility=\'hidden\';\n  }\n}';
+    targ = (document.getElementsByTagName('head'))[0] || document.body;
+    targ.appendChild(scriptNode);
+  }
+
+}).call(this);
